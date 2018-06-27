b54d90e2feffd23931dc5489fec45c5b41086f39
litichevskiy <kunin25@ukr.net>
2018-06-27 15:40:26 +0300

    update server.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const PORT = process.env.PORT || 3000;
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
-const https = require('https');
-const app = express();
+// const PORT = process.env.PORT || 3000;
+// const path = require('path');
+// const fs = require('fs');
+// const express = require('express');
+// const https = require('https');
+// const app = express();
 
-const certOptions = {
-    key: fs.readFileSync('./certificate/server.key'),
-    cert: fs.readFileSync('./certificate/server.crt'),
-}
+// const certOptions = {
+//     key: fs.readFileSync('./certificate/server.key'),
+//     cert: fs.readFileSync('./certificate/server.crt'),
+// }
+
+// app.use('/dist', express.static(__dirname + '/dist'));
+// app.use('/images', express.static(__dirname + '/src/images'));
+// app.use('/workers', express.static(__dirname + '/src/js/workers'));
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname+'/index.html'));
+// });
+
+// https.createServer(certOptions, app)
+//     .listen( PORT, console.log(`server listening on port ${PORT}`))
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
 app.use('/dist', express.static(__dirname + '/dist'));
 app.use('/images', express.static(__dirname + '/src/images'));
@@ -17,5 +31,4 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-https.createServer(certOptions, app)
-    .listen( PORT, console.log(`server listening on port ${PORT}`))
+app.listen( PORT, () => console.log(`server listening on port ${PORT}`));
